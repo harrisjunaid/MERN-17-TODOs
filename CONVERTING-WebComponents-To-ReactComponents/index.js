d301305e6e4afaa32c2762cc7d297356b5a75421
Harris Junaid <harris.junaid@gmail.com>
2023-03-19 14:11:13 -0400

    01 end-a

--- CONVERTING-WebComponents-To-ReactComponents/index.js
+++ CONVERTING-WebComponents-To-ReactComponents/index.js
@@ -23,18 +23,8 @@ function App() {
       <>
         <div className="container">
           <div className="col-sm">
-            {loaded &&
-              data.movies.map((movie, i) => (
-                // Step 3 - Rename '<nexttech-movie' to match the name of your new React Component in 'movies.js'
-                <nexttech-movie key={i}
-                  // Step 2 - Replace all of the attributes below with a single `data` attribute that have `movie` assigned as the value
-                  title={movie.title}
-                  showTime={movie.showTime}
-                  screen={movie.screen}
-                  row={movie.row}
-                  seat={movie.seat}
-                  imageUrl={movie.imageUrl}
-                />
+            {loaded && data.movies.map((movie, i) => (
+                <Movie key={i} data={movie} />
               ))}
           </div>
         </div>
